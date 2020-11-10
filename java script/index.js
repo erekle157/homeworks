cfaebabdb749bab7951c55bcabd68b1a1d5742b8
erekle157 <72881751+erekle157@users.noreply.github.com>
2020-11-10 18:16:46 +0400

    Add files via upload

---
+++ java script/index.js
@@ -0,0 +1,37 @@
+////////////// დავალება 1
+
+// let name = 'erekle'
+
+// let admin = name
+
+///////////// დავალება 2 
+
+let name = 'David'
+console.log(`hello ${1}`) // hello 1
+console.log(`hello ${'name'}`)// hello name
+console.log(`hello ${name}`)// hello devid
+
+/////////////დავალება 3
+
+let x = 25
+if (x < 50) {
+    console.log('Insufficient value')
+} else if (x === 50) {
+    console.log('Exactly matches the meaning')
+} else if (x > 50) {
+    console.log('More than enough is important')
+} else {
+    console.log('A comparison could not be made')
+}
+
+/////////////დავალება 4
+
+const mybd = 2000
+if (2020 - mybd >= 18) {
+    console.log('You are an adult')
+} else if (2020 - mybd <= 18) {
+    console.log('You are a minor')
+}
+
+
+
