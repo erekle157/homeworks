e5ac73159f8b54607953ea8d927a4249b717f9f4
erekle157 <72881751+erekle157@users.noreply.github.com>
2020-11-18 07:12:14 +0400

    Add files via upload

---
+++ java script 2/index.js
@@ -0,0 +1,62 @@
+////////////// დავალება 1
+
+// alert( null || 2 && 3 || 4 ) // 3 false
+
+////////////// დავალება 2
+
+// function checkage(age) {
+//     if (age >= 14 && age <= 90) {
+//         return true
+//     } else {
+//         alert('denied')
+//     }
+// }
+
+// let age = prompt('how old are you');
+
+// if (checkage(age)) {
+//     alert( 'Accept')
+// }
+
+/////////////დავალება 3
+
+// let login = prompt("log in", '')
+
+
+// if (login === 'admin') { 
+//     let pass = prompt('password', '')
+//     if(pass === 'themaster'){
+//         alert('wellcome')
+//     } else if (pass === '' || pass === null) {
+//         alert('canceled')
+//     } else {
+//         alert('wrong password')
+//     }
+// } else if (login === '' || login === null) {
+//     alert('canceled')
+// } else {
+//     alert('I dont know you')
+// }
+
+///////////////დავალება 4
+// function checkAge(age) {
+//     return age > 18 || confirm('Did parents allow you?')
+// }
+// let age = prompt('age please')
+
+// checkAge (age)
+
+///////////////დავალება 5
+
+// let a = prompt("Number 1")
+// let b = prompt("Number 2")
+
+// function minNum(a, b,) {
+//     if(a > b){
+//          return confirm (b)
+//     } else if (b > a){
+//          return confirm(a)
+//     }
+// }
+
+// minNum(a, b,)
